cd86cbacaf80f3a8addb7c95a03a73a7b5a34aa1
Brian Carlson <brian.m.carlson@gmail.com>
2010-03-03 00:58:04 -0600

    got something kinda resembling a pile of trash working well enough to deploy. right.

--- app.rb
+++ app.rb
@@ -1,14 +1,7 @@
 require 'sinatra'
 require 'haml'
 
-class Album
-  attr_accessor :title, :cover_image, :songs
-  def initialize
-    @songs = []
-  end
-end
-
-class Song < Struct.new(:title)
+class Album < Struct.new(:title, :cover_image, :id)
 end
 
 class App < Sinatra::Default
@@ -17,25 +10,13 @@ class App < Sinatra::Default
   get "/" do
     the_southern_front = Album.new
     the_southern_front.title = "The Southern Front"
+    the_southern_front.id = "the-southern-front"
     the_southern_front.cover_image = "dbh_southernfront_medium.jpg"
-    songs = ["Ambush!",
-             "Postcard from Somewhere Cold",
-             "Spit Shine",
-             "Out the Way You Came In",
-             "Married to the Sea",
-             "How to Win an Argument",
-             "5 to 9"].map { |s| Song.new(s) }
-    the_southern_front.songs = songs
-    
+
     five_gallant_steeds = Album.new
     five_gallant_steeds.title = "Five Gallant Steeds"
     five_gallant_steeds.cover_image = "dbh_fivegallantsteeds_medium.jpg"
-    songs = ["Failure",
-             "Goodbye",
-             "Pseudo",
-             "Easy Let Down",
-             "White Lie"].map { |s| Song.new(s) }
-    five_gallant_steeds.songs = songs
+    five_gallant_steeds.id = "five-gallant-steeds.zip"
     @albums = [the_southern_front,five_gallant_steeds]
     haml :index
   end
