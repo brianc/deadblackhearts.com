6f6f21d71e06d66f8822023fb056a9f7ad53f1b5
Brian Carlson <brian.m.carlson@gmail.com>
2010-04-12 23:18:05 -0500

    removed unused app code

--- app.rb
+++ app.rb
@@ -2,23 +2,6 @@ require 'rubygems'
 require 'sinatra'
 require 'haml'
 
-class Album < Struct.new(:title, :cover_image, :id)
-end
-
 class App < Sinatra::Base
-  set :haml, { :format => :html5 }
 
-  get "/" do
-    the_southern_front = Album.new
-    the_southern_front.title = "The Southern Front"
-    the_southern_front.id = "the-southern-front"
-    the_southern_front.cover_image = "dbh_southernfront_medium.jpg"
-
-    five_gallant_steeds = Album.new
-    five_gallant_steeds.title = "Five Gallant Steeds"
-    five_gallant_steeds.cover_image = "dbh_fivegallantsteeds_medium.jpg"
-    five_gallant_steeds.id = "five-gallant-steeds"
-    @albums = [the_southern_front,five_gallant_steeds]
-    haml :index
-  end
 end
